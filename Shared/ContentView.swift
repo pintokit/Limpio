3b91cb6e4d78895718ab41e6326abf9dce3cc0c0
David Solis <github@soliskit.com>
2021-06-12 08:01:06 -0700

    add edit button by including missing navigationView

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -17,21 +17,23 @@ struct ContentView: View {
     private var items: FetchedResults<Item>
 
     var body: some View {
+      NavigationView {
         List {
-            ForEach(items) { item in
-                Text("Item at \(item.timestamp!, formatter: itemFormatter)")
-            }
-            .onDelete(perform: deleteItems)
-        }
-        .toolbar {
-            #if os(iOS)
-            EditButton()
-            #endif
+              ForEach(items) { item in
+                  Text("Item at \(item.timestamp!, formatter: itemFormatter)")
+              }
+              .onDelete(perform: deleteItems)
+          }
+          .toolbar {
+              #if os(iOS)
+              EditButton()
+              #endif
 
-            Button(action: addItem) {
-                Label("Add Item", systemImage: "plus")
-            }
+              Button(action: addItem) {
+                  Label("Add Item", systemImage: "plus")
+              }
         }
+      }
     }
 
     private func addItem() {
