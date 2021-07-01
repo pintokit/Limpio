5ca971a2627f69770a5027346f0daef8a7db788e
David Solis <github@soliskit.com>
2021-06-30 18:09:42 -0700

    formatting

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -9,80 +9,79 @@ import SwiftUI
 import CoreData
 
 struct ContentView: View {
-  @Environment(\.managedObjectContext) private var viewContext
-  
-  @FetchRequest(
-    sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
-    animation: .default)
-  private var items: FetchedResults<Item>
-  
-  var body: some View {
-    NavigationView {
-      List {
-        ForEach(items) { item in
-          if let name = item.name {
-            Text("\(name) at \(item.timestamp!, formatter: itemFormatter)")
-          } else {
-            fatalError("Item name missing")
-          }
-        }
-        .onDelete(perform: deleteItems)
-      }
-      .toolbar {
-        HStack {
+    @Environment(\.managedObjectContext) private var viewContext
+    
+    @FetchRequest(
+        sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)], animation: .default)
+    private var items: FetchedResults<Item>
+    
+    var body: some View {
+        NavigationView {
+            List {
+                ForEach(items) { item in
+                    if let name = item.name, let time = item.timestamp {
+                        Text("\(name) at \(time, formatter: itemFormatter)")
+                    } else {
+                        fatalError("Item name missing")
+                    }
+                }
+                .onDelete(perform: deleteItems)
+            }
+            .toolbar {
+                HStack {
 #if os(iOS)
-          EditButton()
+                    EditButton()
 #endif
-          Button(action: addItem) {
-            Label("Add Item", systemImage: "plus")
-          }
+                    Button(action: addItem) {
+                        Label("Add Item", systemImage: "plus")
+                    }
+                }
+            }
         }
-      }
     }
-  }
-  
-  private func addItem() {
-    withAnimation {
-      let newItem = Item(context: viewContext)
-      newItem.timestamp = Date()
-      newItem.name = "Test"
-      
-      do {
-        try viewContext.save()
-      } catch {
-        // Replace this implementation with code to handle the error appropriately.
-        // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-        let error = error as Error
-        fatalError("Unresolved error \(error), \(error.localizedDescription)")
-      }
+    
+    private func addItem() {
+        withAnimation {
+            let newItem = Item(context: viewContext)
+            newItem.timestamp = Date()
+            newItem.name = "Test"
+            
+            do {
+                try viewContext.save()
+            } catch {
+                // Replace this implementation with code to handle the error appropriately.
+                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+                let error = error as Error
+                fatalError("Unresolved error \(error), \(error.localizedDescription)")
+            }
+        }
     }
-  }
-  
-  private func deleteItems(offsets: IndexSet) {
-    withAnimation {
-      offsets.map { items[$0] }.forEach(viewContext.delete)
-      
-      do {
-        try viewContext.save()
-      } catch {
-        // Replace this implementation with code to handle the error appropriately.
-        // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-        let error = error as Error
-        fatalError("Unresolved error \(error), \(error.localizedDescription)")
-      }
+    
+    private func deleteItems(offsets: IndexSet) {
+        withAnimation {
+            offsets.map { items[$0] }.forEach(viewContext.delete)
+            
+            do {
+                try viewContext.save()
+            } catch {
+                // Replace this implementation with code to handle the error appropriately.
+                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+                let error = error as Error
+                fatalError("Unresolved error \(error), \(error.localizedDescription)")
+            }
+        }
     }
-  }
 }
 
 private let itemFormatter: DateFormatter = {
-  let formatter = DateFormatter()
-  formatter.dateStyle = .short
-  formatter.timeStyle = .medium
-  return formatter
+    let formatter = DateFormatter()
+    formatter.dateStyle = .short
+    formatter.timeStyle = .medium
+    return formatter
 }()
 
 struct ContentView_Previews: PreviewProvider {
-  static var previews: some View {
-    ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
-  }
+    static var previews: some View {
+        ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
+    }
 }
