bdaf1a19e3a4c790b7c0e843b31f48b461aca72e
David Solis <doze.inept.0n@icloud.com>
2022-04-28 13:06:17 -0700

    add watchOS target

---
+++ LimpioWatch WatchKit Extension/LimpioApp.swift
@@ -0,0 +1,21 @@
+//
+//  LimpioApp.swift
+//  LimpioWatch WatchKit Extension
+//
+//  Created by David Solis on 4/28/22.
+//
+
+import SwiftUI
+
+@main
+struct LimpioApp: App {
+    @SceneBuilder var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                WatchRoomListView()
+            }
+        }
+
+        WKNotificationScene(controller: NotificationController.self, category: "myCategory")
+    }
+}
---
+++ LimpioWatch WatchKit Extension/WatchRoomListView.swift
@@ -0,0 +1,21 @@
+//
+//  WatchRoomListView.swift
+//  LimpioWatch WatchKit Extension
+//
+//  Created by David Solis on 4/28/22.
+//
+
+import SwiftUI
+
+struct WatchRoomListView: View {
+    
+    var body: some View {
+        RoomListView()
+    }
+}
+
+struct WatchRoomListView_Previews: PreviewProvider {
+    static var previews: some View {
+        WatchRoomListView()
+    }
+}
