a1d215f47353dc4cbaa8888c8a2fa945074f8e82
David Solis <doze.inept.0n@icloud.com>
2022-04-27 17:21:33 -0700

    update structures to be iterable

--- Limpio/Models/Room.swift
+++ Limpio/Models/Room.swift
@@ -7,7 +7,13 @@
 
 import Foundation
 
-struct Room {
-    let uuid: UUID = UUID()
+struct Room: Identifiable, Codable {
+    let id: UUID
     let name: String
+    
+    init(id: UUID = UUID(),
+         name: String) {
+        self.id = id
+        self.name = name
+    }
 }
--- Limpio/Models/Task.swift
+++ Limpio/Models/Task.swift
@@ -7,10 +7,22 @@
 
 import Foundation
 
-struct Task {
-    let uuid: UUID = UUID()
+struct Task: Identifiable, Codable {
+    let id: UUID
     var name: String
     var participants: [String]
     var frequency: String
     var lastCompletionDate: Date
+    
+    init(id: UUID = UUID(),
+         name: String,
+         participants: [String],
+         frequency: String,
+         lastCompletionDate: Date = Date()) {
+        self.id = id
+        self.name = name
+        self.participants = participants
+        self.frequency = frequency
+        self.lastCompletionDate = lastCompletionDate
+    }
 }
