f8e20d3a11bf2f69ea1676a9816813ae276dab84
David Solis <doze.inept.0n@icloud.com>
2022-04-29 16:26:28 -0700

    add tasks property to Room along with sample data

--- Limpio/Models/Room.swift
+++ Limpio/Models/Room.swift
@@ -10,14 +10,22 @@ import Foundation
 struct Room: Identifiable, Codable {
     let id: UUID
     let name: String
+    var tasks: [Task]
     
     init(id: UUID = UUID(),
-         name: String) {
+         name: String,
+         tasks: [Task]) {
         self.id = id
         self.name = name
+        self.tasks = tasks
     }
 }
 
 extension Room {
-    static let listPreview: [Room] = [Room(name: "Livingroom"), Room(name: "Bedroom"), Room(name: "Kitchen"), Room(name: "Bathroom")]
+    static let listPreview: [Room] = [
+        Room(name: "Livingroom", tasks: [Task(name: "Clean couch", participants: ["Solis, Brandon, Mazza"], frequency: "1")]),
+        Room(name: "Kitchen", tasks: [Task(name: "Empty trash", participants: ["Solis, Brandon, Mazza"], frequency: "3")]),
+        Room(name: "Bathroom", tasks: [Task(name: "Clean shower door", participants: ["Solis, Brandon, Mazza"], frequency: "2")]),
+        Room(name: "Bedroom", tasks: [Task(name: "Make bed", participants: ["Solis"], frequency: "4")])
+    ]
 }
