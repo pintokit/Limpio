5b914b2e119d66d6edf1c2fae9a546a9113eead9
David Solis <doze.inept.0n@icloud.com>
2022-04-29 16:25:56 -0700

    remove room property from tasks along with sample data

--- Limpio/Models/Task.swift
+++ Limpio/Models/Task.swift
@@ -12,24 +12,17 @@ struct Task: Identifiable, Codable {
     var name: String
     var participants: [String]
     var frequency: String
-    var room: Room
     var lastCompletionDate: Date
     
     init(id: UUID = UUID(),
          name: String,
          participants: [String],
          frequency: String,
-         room: Room,
          lastCompletionDate: Date = Date()) {
         self.id = id
         self.name = name
         self.participants = participants
         self.frequency = frequency
-        self.room = room
         self.lastCompletionDate = lastCompletionDate
     }
-}
-
-extension Task {
-    static let listPreview: [Task] = [Task(name: "Empty trash", participants: ["Solis, Brandon, Mazza"], frequency: "3", room: Room(name: "Kitchen")), Task(name: "Clean shower door", participants: ["Solis, Brandon, Mazza"], frequency: "2", room: Room(name: "Bathroom")), Task(name: "Clean couch", participants: ["Solis, Brandon, Mazza"], frequency: "1", room: Room(name: "Livingroom")), Task(name: "Make bed", participants: ["Solis"], frequency: "4", room: Room(name: "Bedroom"))]
 }
