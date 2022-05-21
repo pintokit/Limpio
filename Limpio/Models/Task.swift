8836f61a4ba282b00e61965720723b8f9ecba988
David Solis <doze.inept.0n@icloud.com>
2022-05-21 13:31:47 -0700

    nest Task inside Room struct

--- Limpio/Models/Task.swift
+++ Limpio/Models/Task.swift
@@ -7,27 +7,27 @@
 
 import Foundation
 
-struct Task: Identifiable, Codable {
-    let id: UUID
-    var name: String
-    var participants: [Participant]
-    var frequency: Int
-    var lastCompletionDate: Date
+extension Room {
     
-    init(id: UUID = UUID(),
-         name: String,
-         participants: [String],
-         frequency: Int,
-         lastCompletionDate: Date = Date()) {
-        self.id = id
-        self.name = name
-        self.participants = participants.map { Participant(name: $0) }
-        self.frequency = frequency
-        self.lastCompletionDate = lastCompletionDate
+    struct Task: Identifiable, Codable {
+        let id: UUID
+        var name: String
+        var participants: [Participant]
+        var frequency: Int
+        var lastCompletionDate: Date
+        
+        init(id: UUID = UUID(),
+             name: String,
+             participants: [String],
+             frequency: Int,
+             lastCompletionDate: Date = Date()) {
+            self.id = id
+            self.name = name
+            self.participants = participants.map { Participant(name: $0) }
+            self.frequency = frequency
+            self.lastCompletionDate = lastCompletionDate
+        }
     }
-}
-
-extension Task {
     
     struct Participant: Identifiable, Codable {
         let id: UUID
